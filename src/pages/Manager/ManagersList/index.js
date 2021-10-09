52ae17092b00af23e88d382bbc326f6cf2854ea5
prmacedo <p.ricardomacedo@hotmail.com>
2021-10-09 11:25:49 -0300

    created ManagersList page

--- src/pages/Manager/ManagersList/index.js
+++ src/pages/Manager/ManagersList/index.js
@@ -1,7 +1,73 @@
-import styles from './styles.css';
+import React, { useState } from 'react';
 
-export default function ManagersList() {
+import { FiSearch, FiClipboard, FiUserPlus } from 'react-icons/fi';
+
+import Container from '../../../components/Container';
+
+import styles from './styles.module.css';
+
+export default function ManagerList() {
+  const [search, setSearch] = useState('');
+
+  function handleSearch(evt) {
+    evt.preventDefault();
+  }
+
   return (
-    <h1>Gerentes</h1>
+    <Container>
+      <header className={styles.header}>
+        <h1>
+          <FiClipboard />
+          Gerentes
+        </h1>
+      </header>
+
+      <main className={styles.main}>
+        <div className={styles.toolsRow}>
+          <form onSubmit={handleSearch} className={styles.searchForm}>
+            <input
+              type="text"
+              value={search}
+              onChange={(evt) => setSearch(evt.target.value)}
+              placeholder="Digite o nome ou cargo"
+            />
+
+            <button className={styles.btn}>
+              <FiSearch />
+            </button>
+          </form>
+
+          <div className={styles.btnGroup}>            
+            <button type="button" className={styles.btn}><FiUserPlus /></button>
+          </div>
+        </div>
+        
+        <div className={styles.tableContainer}>
+          <table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Cargo</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>Manoel Augusto</td>
+                <td>Gerente</td>
+                <td>Ativo</td>
+              </tr>
+
+              <tr>
+                <td>Flávia Carneiro</td>
+                <td>Administrador</td>
+                <td>Inativo</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </main>
+    </Container>
   );
 }
