1fd34af8206ab8559ef21923f3e1057e5188ceea
prmacedo <p.ricardomacedo@hotmail.com>
2021-10-15 19:38:39 -0300

    added manager routes

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import ForgotPassword from './pages/ForgotPassword';
 import ClientsLists from './pages/Client/ClientsList';
 import ManagersList from './pages/Manager/ManagersList';
-import Settings from './pages/Settings/Settings';
+import AddManager from './pages/Manager/AddManager';
+import EditManager from './pages/Manager/EditManager';
+import Settings from './pages/Settings';
 
 export default function Routes() {
-  return(
+  return (
     <BrowserRouter>
       <Switch>
         <Route component={Login} path="/" exact />
         <Route component={ForgotPassword} path="/forgotPassword" />
         <Route component={ClientsLists} path="/clients" />
         <Route component={ManagersList} path="/managers" />
+        <Route component={AddManager} path="/addManager" />
+        <Route component={EditManager} path="/manager" />
         <Route component={Settings} path="/settings" />
       </Switch>
     </BrowserRouter>
