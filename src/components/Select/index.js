1cc449075cde1d2da831778d1666f193eccbcad9
prmacedo <p.ricardomacedo@hotmail.com>
2021-10-15 19:03:42 -0300

    added new props and disabled style

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -4,14 +4,17 @@ import ReactSelect from 'react-select';
 
 import styles from './styles.module.css';
 
-export default function Select({ name, options, placeholder, onChange }) {
+export default function Select({ id, name, options, placeholder, onChange, disabled }) {  
   return(
     <ReactSelect
       name={name}
+      id={id}
+      inputId={id}
       options={options}
       placeholder={placeholder}
-      className={styles.select}
+      className={`${styles.select} ${disabled ? styles.disabledSelect : null}`}
       onChange={onChange}
+      isDisabled={disabled}
     />
   )
 }
